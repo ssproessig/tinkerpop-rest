c6d39d300fe45bf75b02831cd56dd77e992b6473
Soeren SPROESSIG <ssproessig@gmail.com>
2019-11-03 15:28:30 +0100

    add basic Spring Boot REST service

---
+++ src/main/java/com/github/ssproessig/tinkerpop/tinkerpop_rest/Application.java
@@ -0,0 +1,16 @@
+package com.github.ssproessig.tinkerpop.tinkerpop_rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
